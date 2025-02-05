ea9b22e6d4628c13a3b202c6a057be75642c1d4d
Dann2405 <danielsoares0558@gmail.com>
2025-02-05 20:25:44 -0300

    feat: biblioteca pio.h adicionada juntamente com a inicialização do WS2812.pio

--- SerialCommunication.c
+++ SerialCommunication.c
@@ -3,14 +3,33 @@
 #include "hardware/spi.h"
 #include "hardware/i2c.h"
 #include "hardware/pio.h"
+#include "hardware/clocks.h"
 
+// pio.h
+#include "ws2812.pio.h"
+void ws2812_init(); // prototipo da inicialização do ws281.pio
+
+// Cabeçalhos .h da pasta include
+#include "include/matrixWS2812.h"
 
 int main()
 {
     stdio_init_all();
+    ws2812_init();
+
+    set_number0(200, 0, 0);
 
     while (true) {
         sleep_ms(500);
         printf("Hello, world!\n");
     }
+}
+
+void ws2812_init()
+{
+    PIO pio = pio0;                                      // Configuração do PIO 0
+    int sm = 0;                                          // Define o estado da máquina de estado do PIO (SM)
+    uint offset = pio_add_program(pio, &ws2812_program); // Vai carregar o programa do PIO para controlar os LEDs WS2812
+
+    ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW); // Vai inicializar o programa de controle do WS2812 no PIO
 }
